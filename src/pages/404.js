38f300ee64e24d71312b83757a4040835fe3938e
Barney Loosemore <barney@barney.co.uk>
2020-04-20 12:44:26 +0100

    Change 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,27 @@
 import React from "react"
+import styled from "styled-components"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/SEO"
 
+const ErrorText = styled.div`
+  text-align: center;
+  font-size: 20px;
+  width: 100%;
+  margin: 32px 0;
+`
+const ErrorHeader = styled(ErrorText)`
+  font-size: 32px;
+  font-weight: bold;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <ErrorHeader>NOT FOUND</ErrorHeader>
+    <ErrorText>
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </ErrorText>
   </Layout>
 )
 
