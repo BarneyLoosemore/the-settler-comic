f7f8769d9b9fbdcd6d43ba7df2ac1d2b461d64ab
Barney Loosemore <barney@barney.co.uk>
2020-04-20 12:45:31 +0100

    Change PageNavigationModal styles

--- src/components/PageList/components/PageNavigationModal/PageNavigationModal.js
+++ src/components/PageList/components/PageNavigationModal/PageNavigationModal.js
@@ -1,81 +1,50 @@
 import React from "react"
-import styled, { css } from "styled-components"
+import {
+  FaAngleLeft,
+  FaAngleRight,
+  FaAngleDoubleLeft,
+  FaAngleDoubleRight,
+} from "react-icons/fa"
 
 import { scrollTo, scrollToPage } from "../../../../utils/scroll"
+import { Container, PageNav, PageNumber } from "./style"
 
-export const PageNavigationModal = ({ pageRefs, activePage }) => {
+export const PageNavigationModal = ({ pageRefs, activePage, visible }) => {
   return (
-    <Container>
-      <div onClick={() => scrollTo(0)}>first</div>
-      <div
+    <Container visible={visible}>
+      <PageNav onClick={() => scrollTo(0)}>
+        <FaAngleDoubleLeft color={activePage <= 1 && "grey"} size={40} />
+      </PageNav>
+      <PageNav
         onClick={() => {
           if (activePage <= 1) return
           const prevPage = pageRefs[activePage - 1]
           scrollToPage(prevPage)
         }}
       >
-        prev
-      </div>
-      <div>{activePage}</div>
-      <div
+        <FaAngleLeft size={40} />
+      </PageNav>
+      <PageNumber>{activePage}</PageNumber>
+      <PageNav
         onClick={() => {
           if (activePage >= pageRefs.length - 1) return
           const nextPage = pageRefs[activePage + 1]
           scrollToPage(nextPage)
         }}
       >
-        next
-      </div>
-      <div
+        <FaAngleRight size={40} />
+      </PageNav>
+      <PageNav
         onClick={() => {
           const lastPage = pageRefs[pageRefs.length - 1]
           scrollToPage(lastPage)
         }}
       >
-        last
-      </div>
+        <FaAngleDoubleRight
+          color={activePage === pageRefs.length - 1 && "grey"}
+          size={40}
+        />
+      </PageNav>
     </Container>
   )
 }
-
-const Container = styled.div`
-  transform: translate(-50%, -50%);
-  left: 50%;
-  position: fixed;
-  bottom: 25px;
-  height: 75px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 999;
-  border-radius: 50px;
-  padding: 5px 40px;
-  opacity: 0.8;
-  color: white;
-  background-color: #1a1a1a;
-  font-size: 35px;
-  font-weight: bold;
-  @media(max-width: 760px){
-    padding 5px 12px;
-    height: 60px;
-    bottom: 0;
-  }
-  div {
-    padding: 0 30px;
-    :hover {
-      cursor: pointer;
-      opacity: 0.7;
-    }
-    @media(max-width: 760px){
-      font-size: 14px;
-      padding: 0 8px
-    }
-  }
-  div:nth-child(3){
-    font-size: 50px;
-    @media(max-width: 760px){
-      font-size: 18px;
-    }
-  }
-}
-`
---
+++ src/components/PageList/components/PageNavigationModal/style.js
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+
+import { TABLET_BREAKPOINT } from "../../../../utils/style"
+
+export const Container = styled.div`
+  transform: translate(-50%, -50%);
+  left: 50%;
+  position: fixed;
+  bottom: 5px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+  border-radius: 50px;
+  padding: 5px 30px;
+  opacity: ${({ visible }) => (visible ? 0.6 : 0)};
+  color: white;
+  background-color: #1a1a1a;
+  font-weight: bold;
+  transition: all 0.3s ease-in-out;
+  :hover {
+    opacity: 1;
+  }
+  @media(max-width: ${TABLET_BREAKPOINT}){
+    padding 5px 12px;
+    height: 60px;
+    bottom: 0;
+    opacity: 0.7;
+    transform: scale(0.75) translate(-68%, -50%);
+    :hover {
+      transform: scale(0.75) translate(-68%, -50%);
+      opacity: 0.7;
+    }
+  }
+
+}
+`
+
+export const PageNav = styled.div`
+  display: flex;
+  align-items: center;
+  :hover {
+    cursor: pointer;
+    opacity: 0.5;
+    transition: all 0.2s ease-in-out;
+  }
+  @media (max-width: ${TABLET_BREAKPOINT}) {
+    font-size: 14px;
+    padding: 0 8px;
+    font-size: 0;
+  }
+`
+
+export const PageNumber = styled.div`
+  font-size: 40px;
+  padding: 0 20px;
+`
