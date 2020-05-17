3203ff70e5baa204eb96e92b8d6dc54458f6b005
Barney Loosemore <barney@barney.co.uk>
2020-05-17 22:12:57 +0100

    Cleanup PageNavigationModal component

--- src/components/PageList/components/PageNavigationModal/PageNavigationModal.js
+++ src/components/PageList/components/PageNavigationModal/PageNavigationModal.js
@@ -1,4 +1,5 @@
 import React from "react"
+import PropTypes from "prop-types"
 import {
   FaAngleLeft,
   FaAngleRight,
@@ -9,42 +10,46 @@ import {
 import { scrollTo, scrollToPage } from "../../../../utils/scroll"
 import { Container, PageNav, PageNumber } from "./style"
 
-export const PageNavigationModal = ({ pageRefs, activePage, visible }) => {
-  return (
-    <Container visible={visible}>
-      <PageNav onClick={() => scrollTo(0)}>
-        <FaAngleDoubleLeft color={activePage <= 1 && "grey"} size={40} />
-      </PageNav>
-      <PageNav
-        onClick={() => {
-          if (activePage <= 1) return
-          const prevPage = pageRefs[activePage - 1]
-          scrollToPage(prevPage)
-        }}
-      >
-        <FaAngleLeft size={40} />
-      </PageNav>
-      <PageNumber>{activePage}</PageNumber>
-      <PageNav
-        onClick={() => {
-          if (activePage >= pageRefs.length - 1) return
-          const nextPage = pageRefs[activePage + 1]
-          scrollToPage(nextPage)
-        }}
-      >
-        <FaAngleRight size={40} />
-      </PageNav>
-      <PageNav
-        onClick={() => {
-          const lastPage = pageRefs[pageRefs.length - 1]
-          scrollToPage(lastPage)
-        }}
-      >
-        <FaAngleDoubleRight
-          color={activePage === pageRefs.length - 1 && "grey"}
-          size={40}
-        />
-      </PageNav>
-    </Container>
-  )
+export const PageNavigationModal = ({ pageRefs, activePage, visible }) => (
+  <Container visible={visible}>
+    <PageNav onClick={() => scrollTo(0)}>
+      <FaAngleDoubleLeft color={activePage <= 1 && "grey"} size={40} />
+    </PageNav>
+    <PageNav
+      onClick={() => {
+        if (activePage <= 1) return
+        const prevPage = pageRefs[activePage - 1]
+        scrollToPage(prevPage)
+      }}
+    >
+      <FaAngleLeft size={40} />
+    </PageNav>
+    <PageNumber>{activePage}</PageNumber>
+    <PageNav
+      onClick={() => {
+        if (activePage >= pageRefs.length - 1) return
+        const nextPage = pageRefs[activePage + 1]
+        scrollToPage(nextPage)
+      }}
+    >
+      <FaAngleRight size={40} />
+    </PageNav>
+    <PageNav
+      onClick={() => {
+        const lastPage = pageRefs[pageRefs.length - 1]
+        scrollToPage(lastPage)
+      }}
+    >
+      <FaAngleDoubleRight
+        color={activePage === pageRefs.length - 1 && "grey"}
+        size={40}
+      />
+    </PageNav>
+  </Container>
+)
+
+PageNavigationModal.propTypes = {
+  pageRefs: PropTypes.array.isRequired,
+  activePage: PropTypes.number.isRequired,
+  visible: PropTypes.bool,
 }
