0db80b11882e4c5741a212f751bf97f6399ddd2b
Barney Loosemore <barney@barney.co.uk>
2020-05-17 22:16:48 +0100

    Fix graphql warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/SEO"
