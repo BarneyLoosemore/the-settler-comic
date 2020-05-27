f1407c1e148abcf2c909a4870f317774c3594e65
Barney Loosemore <barney@barney.co.uk>
2020-05-27 16:45:19 +0100

    Add smoothscroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,7 @@
 const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
 const { linkResolver } = require("./src/utils/linkResolver")
+require("smoothscroll-polyfill").polyfill()
+
+window.__forceSmoothScrollPolyfill__ = true
 
 registerLinkResolver(linkResolver)
