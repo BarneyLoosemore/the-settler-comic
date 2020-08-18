cfdac70bc1c7811ca43761e7fdc944997f4d456f
Barney Loosemore <barney@barney.co.uk>
2020-08-18 16:35:35 +0100

    Add LoadingSpinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import "./index.css"
+
+export const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="lds-ellipsis">
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+)
